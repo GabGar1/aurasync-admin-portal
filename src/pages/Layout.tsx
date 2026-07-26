419f1d02f04ce206e71354aed147c8eeea372edd
GabGar1 <gabrielgarbrecht@outlook.com>
2026-07-26 16:43:25 -0300

    fix: make main content fill viewport height with overflow scroll

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -85,7 +85,7 @@ export default function Layout() {
             {sidebarItems.find((i) => location.pathname === i.url || location.pathname.startsWith(i.url + "/"))?.title || "AuraSync"}
           </span>
         </header>
-        <main className="p-6">
+        <main className="flex-1 overflow-y-auto p-6">
           <Outlet />
         </main>
         <Toaster />
